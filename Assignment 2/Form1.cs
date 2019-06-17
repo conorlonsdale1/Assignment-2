05c486492e4394cf0766baf4354a80bd078bd9d2
rc0032659 <rc0032659@CRO-24-10>
2019-06-17 14:45:20 +0100

    Save 1

--- Assignment 2/Form1.cs
+++ Assignment 2/Form1.cs
@@ -16,5 +16,93 @@ namespace Assignment_2
         {
             InitializeComponent();
         }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double R1, R2, R3, R4, R5, seriesTotal, parallelTotal;
+            try
+            {
+                R1 = Convert.ToDouble(textBox1.Text);
+            }
+            catch
+            {
+                R1 = 0;
+            }
+            try
+            {
+                R2 = Convert.ToDouble(textBox2.Text);
+            }
+            catch
+            {
+                R2 = 0;
+            }
+            try
+            {
+                R3 = Convert.ToDouble(textBox3.Text);
+            }
+            catch
+            {
+                R3 = 0;
+            }
+            try
+            {
+                R4 = Convert.ToDouble(textBox4.Text);
+            }
+            catch
+            {
+                R4 = 0;
+            }
+            try
+            {
+                R5 = Convert.ToDouble(textBox5.Text);
+            }
+            catch
+            {
+                R5 = 0;
+            }
+            seriesTotal = R1 + R2 + R3 + _R4 + R5;
+            label6.Text = "seriesTotal =" + seriesTotal;
+
+            if (R1 = 0)
+            {
+                R1 = double.PositiveInfinity;
+            }
+            if (R2 = 0)
+            {
+                R2 = double.PositiveInfinity;
+            }
+            if (R3 = 0)
+            {
+                R3 = double.PositiveInfinity;
+            }
+            if (R4 = 0)
+            {
+                R4 = double.PositiveInfinity;
+            }
+            if (R5 = 0)
+            {
+                R5 = double.PositiveInfinity;
+            }
+        }
+
+        private void textBox5_TextChanged(object sender, EventArgs e)
+        {
+
+        }
     }
 }
